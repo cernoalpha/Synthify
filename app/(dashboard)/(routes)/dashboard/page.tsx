7d8fb6f1107ed7c3f45cb628a1425f53d9b134c7
cernoalpha <mirshahzadmubeen1@gmail.com>
2024-05-14 19:10:52 +0530

    Changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
-   <Button>Click Me</Button>
+   <div>
+    Dashboard (protected)
+    <UserButton afterSignOutUrl="/"/>
+   </div>
   );
 }
