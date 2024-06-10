315a35aa84e20ecac9554a26ad72679277fa8a78
Mohammed Armaan <armaan0243@gmail.com>
2024-06-10 17:19:36 +0530

    nah

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -5,7 +5,7 @@ const settings = () => {
     <>
     <div className="container mx-auto p-6">
       <h1 className="text-2xl font-bold mb-4">Settings</h1>
-        {/* <SettingsForm /> */}
+      
     </div>
     
     </>
