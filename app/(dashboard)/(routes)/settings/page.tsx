7886ac734dee38ade6d11971f44b682fd6cca0cd
Mohammed Armaan <armaan0243@gmail.com>
2024-06-10 17:19:15 +0530

    First settings commit

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const settings = () => {
+  return (
+    <>
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">Settings</h1>
+        {/* <SettingsForm /> */}
+    </div>
+    
+    </>
+  )
+}
+
+export default settings
